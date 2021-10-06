a626b0da69ddcb42c590e7d05b625f19f8b95e37
Abhinav Sharma <as567652@gmail.com>
2021-10-06 09:17:12 +0530

    Added Data update on server side using Queue + Multithreading

--- main/server.py
+++ main/server.py
@@ -1,26 +1,68 @@
+import tkinter as tk
 import time, socket, sys
-import csv
+import csv, os
 from _thread import *
+import threading
+from queue import Queue
+from watchdog.observers import Observer
+from watchdog.events import LoggingEventHandler
 
-
-filename1 = "CSV_Files/questionaire.csv"
-filename2 = "CSV_Files/id_passwords.csv"
+# All data will go here
 QQ = []
 PP = dict()
-with open(filename1, 'r') as csvfile:
-    csvreader = csv.reader(csvfile)
-    fields = next(csvreader)
-    for row in csvreader:
-        mydict = {}
-        for i in range(len(row)):
-            mydict[fields[i]] = row[i]
-        QQ.append(mydict)
+RR = []
 
-with open(filename2, 'r') as csvfile:
-	csvreader = csv.reader(csvfile)
-	fields = next(csvreader)
-	for row in csvreader:
-		PP[row[0]] = row[1]
+q = Queue()
+QUEUE_LENGTH = 0
+
+class Event(LoggingEventHandler):
+    def on_modified(self, event):
+        push_in_Q(QUEUE_LENGTH)
+
+def setting_up_window(window):
+    #window.attributes('-fullscreen', True)
+    window.title("Server Side")
+
+    screen_width = window.winfo_screenwidth()
+    screen_height = window.winfo_screenheight()
+    window.geometry(f"{screen_width//2}x{screen_height//2}")
+    #window.resizable(False, False)
+
+
+def setting_up_csv():
+    filename1 = "CSV_Files/questionaire.csv"
+    filename2 = "CSV_Files/id_passwords.csv"
+
+    csvreader1 = csv.DictReader(open(filename1))
+    for row in csvreader1:
+        QQ.append(row)
+
+    with open(filename2, 'r') as csvfile:
+        csvreader2 = csv.reader(csvfile)
+        for row in csvreader2:
+            PP[row[0]] = row[1]
+    push_in_Q(QUEUE_LENGTH)
+
+
+def push_in_Q(QUEUE_LENGTH):
+    filename3 = "CSV_Files/progress.csv"
+    csvreader3 = csv.DictReader(open(filename3))
+    for row in csvreader3:
+        RR.append(row)
+    q.put(RR)
+    QUEUE_LENGTH+=1
+
+
+def update_csv():
+    filename3 = "CSV_Files/progress.csv"
+    with open(filename3, 'r') as csvfile:
+        csvreader = csv.reader(csvfile)
+        RR_fields = next(csvreader)
+    with open(filename3, 'w') as csvfile:
+        writer = csv.DictWriter(csvfile, fieldnames = RR_fields)
+        writer.writeheader()
+        writer.writerows(RR)
+    
 
 def threaded_client(conn):
 	try:
@@ -50,38 +92,100 @@ def threaded_client(conn):
 	except:
 		return
 
-def server_program():
-	# get the hostname
-	host = socket.gethostname()
-	#host = '127.0.0.1'
-	port = 8008  # initiate port no above 1024
-	
-	server_socket = socket.socket()  # get instance
-	# look closely. The bind() function takes tuple as argument
-	server_socket.bind((host, port))  # bind host address and port together
-	
+
+def setting_up_server():
+    # get the hostname
+    host = socket.gethostname()
+    #host = '127.0.0.1'
+    port = 8008  # initiate port no above 1024
+    server_socket = socket.socket()  # get instance
+    # start the server again if you shut it down, instead of waiting for a minute for TIME_WAIT
+    server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+    # look closely. The bind() function takes tuple as argument
+    server_socket.bind((host, port))  # bind host address and port together
+    return server_socket
+
+
+def start_server(server_socket):
 	# configure how many client the server can listen simultaneously
-	server_socket.listen(60)
-	while True:
-		conn, address = server_socket.accept()  # accept new connection	
-		s1 = "Enter Your Register Number :- "
-		conn.send(s1.encode())
-		reg_no = conn.recv(1024).decode()
-		time.sleep(0.1)
-		s2 = "Enter Your Password :- "
-		conn.send(s2.encode())
-		passw = conn.recv(1024).decode()
+    server_socket.listen(60)
+    while True:
+        conn, address = server_socket.accept()  # accept new connection	
+        s1 = "Enter Your Register Number :- "
+        conn.send(s1.encode())
+        reg_no = conn.recv(1024).decode()
+        time.sleep(0.1)
+        s2 = "Enter Your Password :- "
+        conn.send(s2.encode())
+        passw = conn.recv(1024).decode()
 
-		if (reg_no in PP and PP[reg_no] == passw):
-			allowed = "ALLOWED"
-			conn.send(allowed.encode())
-			print(f'Registration Number - {reg_no} has started the test.')
-		else:
-			not_allowed = "NOT ALLOWED"
-			conn.send(not_allowed.encode())
+        if (reg_no in PP and PP[reg_no] == passw):
+            for i in RR:
+                if i['id'] == reg_no and i['started'] == 'true':
+                    print('Already Started')
+                    break
+                elif i['id'] == reg_no and i['started'] == 'false':
+                    i['started'] = 'true'
+                    update_csv()
+                    break
+            allowed = "ALLOWED"
+            conn.send(allowed.encode())
+            
+            print(f'Registration Number - {reg_no} has started the test.')
+        else:
+            not_allowed = "NOT ALLOWED"
+            conn.send(not_allowed.encode())
 
-		start_new_thread(threaded_client, (conn, ))
+        start_new_thread(threaded_client, (conn, ))
+
+
+def display(window):
+        label = tk.Label(window, text=q.get()[0])
+        label.pack()
+
+def chec(window):
+    if QUEUE_LENGTH != q.qsize():
+        display(window)
+
+def t0():
+    window = tk.Tk()
+    setting_up_window(window)
+    display(window)
+    B = tk.Button(window, text ="Refresh", command=lambda: chec(window))
+    B.pack()
+    window.mainloop()
+
+
+def t1():
+    setting_up_csv()
+    server_socket = setting_up_server()
+    start_server(server_socket)
+
+
+def t3():
+    path = 'CSV_Files/progress.csv'
+    # Initialize logging event handler
+    event_handler = Event()
+    # Initialize Observer
+    observer = Observer()
+    observer.schedule(event_handler, path, recursive=True)
+    # Start the observer
+    observer.start()
+    try:
+        while True:
+            # Set the thread sleep time
+            time.sleep(2)
+    except KeyboardInterrupt:
+        observer.stop()
+    observer.join()
 
 
 if __name__ == '__main__':
-	server_program()
+    thread_0 = threading.Thread(target=t0)
+    thread_1 = threading.Thread(target=t1)
+    thread_3 = threading.Thread(target=t3)
+    thread_0.start()
+    thread_1.start()
+    thread_3.start()
+    # thread_0.join()
+    # thread_1.join()
