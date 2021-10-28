49473cb59f7e98ce2b9685ba33239cf2db1bc568
Abhinav Sharma <as567652@gmail.com>
2021-10-29 00:24:48 +0530

    Final Draft

--- main/client.py
+++ main/client.py
@@ -37,7 +37,7 @@ class App(tk.Tk):
 		frame0 = tk.LabelFrame(self, text="Instructions")
 		frame0.place(height=100, width=self.W - 50,rely=0.06,relx=0.02)
 		
-		label2 = Label(frame0, bg='#FFFFFF', text="1. Enter your Registration Number and Password provided by respective Course Faculty.											", font=('Helvetica bold', 10))
+		label2 = Label(frame0, bg='#FFFFFF', text="1. Enter your Registration Number and Password (provided by respective course faculty).										", font=('Helvetica bold', 10))
 		label2.place(height=20, width=self.W - 100, rely=0.05,relx=0.015)
 		label3 = Label(frame0, bg='#FFFFFF', text="2. Do not close window during test. This test is only One-Time Accessible.												", font=('Helvetica bold', 10))
 		label3.place(height=20, width=self.W - 100, rely=0.3,relx=0.015)
@@ -76,33 +76,35 @@ class App(tk.Tk):
 		frame0 = tk.LabelFrame(self.frame1, text="Fill Your LogIn Details Here")
 		frame0.place(height=320, width=self.W - 200, rely=0.05, relx=0.055)
 
-		image = Image.open("images/login_img.jpg")
-		resize_image = image.resize((300, 250))
+		image = Image.open("images/login_img2.jpg")
+		resize_image = image.resize((350, 250))
 		img = ImageTk.PhotoImage(resize_image)
 		# create label and add resize image
 		label1 = Label(frame0, image=img)
 		label1.image = img
-		label1.place(height=250, width=300,rely=0.075,relx=0.05)
+		label1.place(height=250, width=350,rely=0.075,relx=0.03)
 
-		self.message_label = tk.Label(frame0, text = '', font=('Helvetica bold', 10)) 
-		self.message_label.place(rely=0.1,relx=0.4)
+		left_margin = 0.425
+
+		self.message_label = tk.Label(frame0, text = '', font=('Helvetica bold', 10))
+		self.message_label.place(rely=0.1,relx=left_margin)
 
 		name_label = tk.Label(frame0, text = 'Student Registration Number', font=('Helvetica bold', 11))  
-		name_label.place(rely=0.2,relx=0.4)
+		name_label.place(rely=0.2,relx=left_margin)
 		name_entry = tk.Entry(frame0, textvariable = self.name_var, font=('Helvetica bold', 11,'normal'), width=60)
-		name_entry.place(rely=0.3,relx=0.4)
+		name_entry.place(rely=0.3,relx=left_margin)
 
-		passw_label = tk.Label(frame0, text = 'Password', font = ('Helvetica bold', 11))
-		passw_label.place(rely=0.4,relx=0.4)
+		passw_label = tk.Label(frame0, text = 'Password (as provided by respective course faculty)', font = ('Helvetica bold', 11))
+		passw_label.place(rely=0.4,relx=left_margin)
 		passw_entry=tk.Entry(frame0, textvariable = self.passw_var, font = ('Helvetica bold', 11,'normal'), width=60)
-		passw_entry.place(rely=0.5,relx=0.4)
+		passw_entry.place(rely=0.5,relx=left_margin)
 		self.show_hide(passw_entry)
 
 		c1 = tk.Checkbutton(frame0, text=' (Hide Password)',variable=self.var1, onvalue=1, offvalue=0, command= lambda: [self.show_hide(passw_entry)])
-		c1.place(rely=0.6,relx=0.4)
+		c1.place(rely=0.6,relx=left_margin)
 
 		login_btn=tk.Button(frame0, text = 'Log In', command = self.submit)
-		login_btn.place(rely=0.7,relx=0.4)
+		login_btn.place(rely=0.7,relx=left_margin)
 
 
 	def win1(self):
@@ -117,7 +119,7 @@ class App(tk.Tk):
 		label3.place(height=20, width=self.W - 100, rely=0.25,relx=0.015)
 		label4 = Label(frame2, bg='#FFFFFF', text="3. Review window is availaible at the end of the test. Submit Button will be enabled only when all questions are attempted.							", font=('Helvetica bold', 10))
 		label4.place(height=20, width=self.W - 100, rely=0.45,relx=0.015)
-		label5 = Label(frame2, bg='#FFFFFF', text="4. There are no partial marks and no negative marks.														", font=('Helvetica bold', 10))
+		label5 = Label(frame2, bg='#FFFFFF', text="4. There are no partial marks and no negative marking.														", font=('Helvetica bold', 10))
 		label5.place(height=20, width=self.W - 100, rely=0.65,relx=0.015)
 
 		self.login_win()
@@ -163,9 +165,29 @@ class App(tk.Tk):
 			os._exit(0)
 
 
+	def UPDATE_REVIEW(self):
+		#tree view
+		new_frame = tk.LabelFrame(self.review, text="Current Answer Status")
+		new_frame.place(height=255, width=425,rely=0.047,relx=0.02)
+
+		tree = ttk.Treeview(new_frame, column=("c1", "c2"), show='headings', height=10)
+		headings = ['Question Number', 'Status']
+		for i in range(len(headings)):
+			tree.column(f"# {i + 1}", anchor=CENTER)
+			tree.heading(f"# {i + 1}", text=headings[i])
+
+		for i in range(1, 11):
+			s = self.ANS_STR[i].get()
+			if s == "":
+				tree.insert('', 'end', text=i, values=('Q.' + str(i), "Not attempted yet."))
+			else:
+				tree.insert('', 'end', text=i, values=('Q.' + str(i), "Answer Saved"))
+		tree.pack()
+
+
 	def win2(self):
 		style = ttk.Style()
-		style.configure('TNotebook.Tab', padding = [35, 8])
+		style.configure('TNotebook.Tab', padding = [35, 8], background = [("white")])
 
 		notebook = ttk.Notebook(self.frame1)
 		FRAME_LIST = []
@@ -180,29 +202,37 @@ class App(tk.Tk):
 		for i in self.questionaire:
 			REL_Y = 0.1
 			frame = FRAME_LIST[q_no - 1]
-			name_label = tk.Label(frame, text = 'Q' + str(q_no) + '. ' + i['ques'], font=('Helvetica bold', 12))
+
+			c = Canvas(frame, bg="white", height=250, width=self.W - 250)
+			c.place(rely=0.05, relx=0.025)
+
+			name_label = tk.Label(frame, text = 'Q' + str(q_no) + '. ' + i['ques'], font=('Helvetica bold', 12), background="white")
 			name_label.place(rely=REL_Y,relx=0.1)
 
 			v = StringVar("")
 			REL_Y += 0.20
-			r = Radiobutton(frame, text = i['Option1'], variable = self.ANS_STR[q_no], value = 'A', font=('Helvetica bold', 12), command=self.ENABLE_MY_BUTTON)
+			r = Radiobutton(frame, text = i['Option1'], variable = self.ANS_STR[q_no], value = 'A', background="white", border=0, font=('Helvetica bold', 12), command= lambda : [self.ENABLE_MY_BUTTON(), self.UPDATE_REVIEW()])
 			r.place(rely=REL_Y, relx=0.1)
 			REL_Y += 0.15
-			r = Radiobutton(frame, text = i['Option2'], variable = self.ANS_STR[q_no], value = 'B', font=('Helvetica bold', 12), command=self.ENABLE_MY_BUTTON)
+			r = Radiobutton(frame, text = i['Option2'], variable = self.ANS_STR[q_no], value = 'B', background="white", font=('Helvetica bold', 12), command= lambda : [self.ENABLE_MY_BUTTON(), self.UPDATE_REVIEW()])
 			r.place(rely=REL_Y, relx=0.1)
 			REL_Y += 0.15
-			r = Radiobutton(frame, text = i['Option3'], variable = self.ANS_STR[q_no], value = 'C', font=('Helvetica bold', 12), command=self.ENABLE_MY_BUTTON)
+			r = Radiobutton(frame, text = i['Option3'], variable = self.ANS_STR[q_no], value = 'C', background="white", font=('Helvetica bold', 12), command= lambda : [self.ENABLE_MY_BUTTON(), self.UPDATE_REVIEW()])
 			r.place(rely=REL_Y, relx=0.1)
 			REL_Y += 0.15
-			r = Radiobutton(frame, text = i['Option4'], variable = self.ANS_STR[q_no], value = 'D', font=('Helvetica bold', 12), command=self.ENABLE_MY_BUTTON)
+			r = Radiobutton(frame, text = i['Option4'], variable = self.ANS_STR[q_no], value = 'D', background="white", font=('Helvetica bold', 12), command= lambda : [self.ENABLE_MY_BUTTON(), self.UPDATE_REVIEW()])
 			r.place(rely=REL_Y, relx=0.1)
 			q_no += 1
 
 		review = ttk.Frame(notebook)
 		notebook.add(review, text='Review')
+		c = Canvas(review, bg="white", height=250, width=self.W - 250)
+		c.place(rely=0.05, relx=0.02)
+		self.review = review
+		self.UPDATE_REVIEW()
 
-		self.sub_btn=tk.Button(review, text = 'Submit Your Answer', command = self.send_it, state=DISABLED)
-		self.sub_btn.place(rely=REL_Y,relx=0.5)
+		self.sub_btn=tk.Button(review, text = 'Submit Your Answers And Exit', command = self.send_it, state=DISABLED)
+		self.sub_btn.place(rely=0.4,relx=0.62)
 
 
 	def ask_to_quit(self):
